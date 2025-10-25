373dce5181bd8240381aaa153608a632065761d0
Hridyanshsingh <hridyanshsingh2006@gmail.com>
2025-10-25 16:42:24 +0530

    Update Project.sol
    Wrote a small contract showing the project name and description

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -1,6 +1,20 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
 
-pragma solidity >=0.8.2 <0.9.0;
+contract Identity {
+    string Project;
+    string Description;
 
-contract Music-Rights-NFT-Marketplace {
+    constructor() {
+        Project = "Music-Rights-NFT-Marketplace";
+        Description = unicode"A Music Rights NFT Marketplace is a blockchain-powered platform that allows artists to tokenize their music rights as NFTs, enabling secure and transparent ownership, royalty distribution, and resale tracking. It empowers creators to monetize their work directly, while fans and investors can buy, trade, and earn from verified music assets.";
+    }
+    
+    function showProject() public view returns (string memory) {
+        return Project;
+    }
+    
+    function showDescription() public view returns (string memory) {
+        return Description; 
+    }
 }
