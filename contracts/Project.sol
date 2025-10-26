a70fc04f6e6076f375d5fde3dc705128bbf62d31
Hridyanshsingh <hridyanshsingh2006@gmail.com>
2025-10-26 20:06:48 +0530

    Update Project.sol
    added startdate

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -4,10 +4,12 @@ pragma solidity >=0.5.0 <0.9.0;
 contract Identity {
     string Project;
     string Description;
+    int startdate;
 
     constructor() {
         Project = "Music-Rights-NFT-Marketplace";
         Description = unicode"A Music Rights NFT Marketplace is a blockchain-powered platform that allows artists to tokenize their music rights as NFTs, enabling secure and transparent ownership, royalty distribution, and resale tracking. It empowers creators to monetize their work directly, while fans and investors can buy, trade, and earn from verified music assets.";
+        startdate = 23102006;
     }
     
     function showProject() public view returns (string memory) {
@@ -16,5 +18,8 @@ contract Identity {
     
     function showDescription() public view returns (string memory) {
         return Description; 
+    }
+    function showstartDate() public view returns (int memory) {
+        return startdate; 
     }
 }
